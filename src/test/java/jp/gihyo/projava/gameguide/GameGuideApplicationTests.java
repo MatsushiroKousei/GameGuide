171c1d877157e859212465ec80744c3cf8f631fb
Matsushiro <matsusiro12kousei@gmail.com>
2023-09-06 10:59:13 +0900

    GameGuideコミット全チェック

---
+++ src/test/java/jp/gihyo/projava/gameguide/GameGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.gihyo.projava.gameguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
