4acc3ca7df82b458c37bb01197360377e4377e05
cross03kohei <140483428+cross03kohei@users.noreply.github.com>
2023-09-06 13:10:58 +0900

    Repositoryの作成

---
+++ src/main/java/jp/gihyo/projava/gameguide/repository/ImageRepository.java
@@ -0,0 +1,7 @@
+package jp.gihyo.projava.gameguide.repository;
+
+import jp.gihyo.projava.gameguide.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image,Integer> {
+}
