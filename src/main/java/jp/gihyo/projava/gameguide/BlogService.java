9ed5a6a575afa77694178027d103b70af90d3f72
Matsushiro <matsusiro12kousei@gmail.com>
2023-09-08 09:54:34 +0900

    一覧表示の機能実装

--- src/main/java/jp/gihyo/projava/gameguide/BlogService.java
+++ src/main/java/jp/gihyo/projava/gameguide/BlogService.java
@@ -1,9 +1,12 @@
 package jp.gihyo.projava.gameguide;
 
+import jp.gihyo.projava.gameguide.entity.Blog;
 import jp.gihyo.projava.gameguide.repository.BlogRepository;
 import jp.gihyo.projava.gameguide.repository.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BlogService {
@@ -12,4 +15,8 @@ public class BlogService {
 
     @Autowired
     ImageRepository imageRepository;
+
+    List<Blog> blogGetAll(){
+       return blogRepository.findAll();
+    }
 }
--- src/main/java/jp/gihyo/projava/gameguide/HomeController.java
+++ src/main/java/jp/gihyo/projava/gameguide/HomeController.java
@@ -1,15 +1,24 @@
 package jp.gihyo.projava.gameguide;
 
 
+import jp.gihyo.projava.gameguide.entity.Blog;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
-//    @GetMapping("/index")
-//    String index(Model model){
-//
-//    }
+    @Autowired
+    BlogService service;
+    @GetMapping("/index")
+    String index(Model model){
+        List<Blog> blogs = service.blogGetAll();
+        model.addAttribute("blogs" , blogs);
+    return "index";
+    }
+
 }
