4d298a5c41bc79687918978aa82a77b7867e8bb9
Matsushiro <matsusiro12kousei@gmail.com>
2023-09-13 15:26:40 +0900

    top3_Matsuにて、newblog新規順で表示成功

--- src/main/java/jp/gihyo/projava/gameguide/HomeController.java
+++ src/main/java/jp/gihyo/projava/gameguide/HomeController.java
@@ -16,32 +16,44 @@ import java.util.List;
 public class HomeController {
     @Autowired
     BlogService service;
-    @GetMapping("/index")
-    String index(Model model){
-        List<Blog> blogs = service.getBlogTop3();
-        Blog top1= blogs.get(0);
-        Blog top2= blogs.get(1);
-        Blog top3= blogs.get(2);
 
-        model.addAttribute("blogs" , blogs);
-        model.addAttribute("top1",top1);
-        model.addAttribute("top2",top2);
-        model.addAttribute("top3",top3);
-    return "index";
+    @GetMapping("/index")
+    String index(Model model) {
+        List<Blog> blogs = service.getBlogTop3();
+        Blog top1 = blogs.get(0);
+        Blog top2 = blogs.get(1);
+        Blog top3 = blogs.get(2);
+
+        List<Blog> blogs1 = service.BlogDate();
+        Blog Date1 = blogs1.get(0);
+        Blog Date2 = blogs1.get(1);
+        Blog Date3 = blogs1.get(2);
+
+        model.addAttribute("blogs", blogs);
+        model.addAttribute("top1", top1);
+        model.addAttribute("top2", top2);
+        model.addAttribute("top3", top3);
+        model.addAttribute("Date1", Date1);
+        model.addAttribute("Date2", Date2);
+        model.addAttribute("Date3", Date3);
+
+        return "index";
     }
+
     @GetMapping("/postblog")
-    String postblog(Model model){
-        model.addAttribute("blogRequest" , new BlogRequest());
+    String postblog(Model model) {
+        model.addAttribute("blogRequest", new BlogRequest());
         return "postblog";
     }
+
     @RequestMapping("/blog/add")
-    String post(Model model, @ModelAttribute BlogRequest blogRequest){
+    String post(Model model, @ModelAttribute BlogRequest blogRequest) {
         service.create(blogRequest);
         return "redirect:/index";
     }
+
     @GetMapping("/search")
-    String search(Model model){
-        model.addAttribute("blogRequest" , new BlogRequest());
+    String search(Model model) {
         return "search";
     }
 }
