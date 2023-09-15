f5828f4723b07a50fbb5663d631c845a789b58e4
hirakawa <hirakawakeiko0915@gmail.com>
2023-09-15 15:20:02 +0900

    作成途中

--- src/main/java/jp/gihyo/projava/gameguide/BlogService.java
+++ src/main/java/jp/gihyo/projava/gameguide/BlogService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class BlogService {
@@ -37,4 +38,24 @@ public class BlogService {
         blog.setCreatedDate(new Date());
         return blog;
     }
+//    public <LIst> List<HomeController.SearchItem> search(Integer id,String title,String text,Integer viewCount){
+//        String query = "SELECT * FROM blog WHERE search_data like '" + id + "'%' AND title '%"+ text + "%' AND '%"+viewCount+"%'";
+//        if(done.equals("true")){
+//            query += " AND done = true";
+//        }
+//        if(done.equals("false")){
+//            query += " AND done = false";
+//        }
+//        List<Map<String, Object>> result = this.blogRepository.queryForList(query);
+//        List<HomeController.SearchItem> list = result.stream().map(
+//                (Map<String, Object> row) -> new HomeController.SearchItem(
+//                        row.get("id").toString(),
+//                        row.get("title").toString(),
+//                        row.get("text").toString(),//,
+//                        row.get("viewCount").toString()
+////                        (Boolean)row.get("done")
+//
+//                )).toList();
+//        return list;
+//    }
 }
--- src/main/java/jp/gihyo/projava/gameguide/HomeController.java
+++ src/main/java/jp/gihyo/projava/gameguide/HomeController.java
@@ -5,17 +5,18 @@ import jp.gihyo.projava.gameguide.entity.Blog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.parser.Entity;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HomeController {
     @Autowired
     BlogService service;
+    record SearchItem(Integer id,String title,String text,Integer viewCount){}
+    private List<SearchItem> searchItems = new ArrayList<>();
     @GetMapping("/index")
     String index(Model model){
         List<Blog> blogs = service.blogGetAll();
@@ -34,5 +35,19 @@ public class HomeController {
         service.create(blogRequest);
         return "redirect:/index";
     }
+
+    @GetMapping("/search")
+    String listItems(Model model){
+        model.addAttribute("SearchList",searchItems);
+        return "search";
+    }
+//    <form action="/search">
+//<!--            <label>新着順</label><input type="radio" name="order" value="新着順" checked>-->
+//<!--            <label>人気順</label><input type="radio" name="order" value="人気順">-->
+//            <input type="text" id="search" name="search" size="20" /><button>検索</button>
+//<!--            <label>AND検索</label><input type="radio" name="keyword" value="AND" checked>-->
+//<!--            <label>OR検索</label><input type="radio" name="keyword" value="OR">-->
+//<!--            <label>NOT検索</label><input type="radio" name="keyword" value="NOT">-->
+//        </form>
 
 }
