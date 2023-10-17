98e4fb64c013bfec11da0654b602ab17814c9bd5
Matsushiro <matsusiro12kousei@gmail.com>
2023-10-17 14:14:30 +0900

    コミット用

--- src/main/java/jp/gihyo/projava/gameguide/BlogRequest.java
+++ src/main/java/jp/gihyo/projava/gameguide/BlogRequest.java
@@ -1,11 +1,9 @@
 package jp.gihyo.projava.gameguide;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 @Data
 public class BlogRequest {
+
     private String title;
 
     private String contents;
