6ed99debc57f7c527b33d79c0be1861ff36fc51a
foursixcross <shimutacross@gmail.com>
2023-10-20 14:26:34 +0900

    途中

---
+++ src/main/resources/static/js/slick-conf.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+$(function() {
+    $('.slick').slick({
+        fade: true,
+        autoplay: true,
+        speed: 1500,
+        autoplaySpeed : 2000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        arrows: false,
+    })
+});</script>
