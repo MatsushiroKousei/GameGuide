057fa7d31aa93ce0114d34376123bd2549723cf7
Matsushiro <matsusiro12kousei@gmail.com>
2023-11-01 09:58:12 +0900

    コメントの追加とバリデーション文字制限追加

--- src/main/java/jp/gihyo/projava/gameguide/BlogRequest.java
+++ src/main/java/jp/gihyo/projava/gameguide/BlogRequest.java
@@ -11,14 +11,15 @@ public class BlogRequest {
     @NotNull　->nullのみ×
     @NotEmpty ->空白のみ〇
     Validationで制限する文字、これらをtitleとcontentsに付与することにより、投稿に制限を掛ける
+    @Size(min = "", message=" ") ->Validationに文字数制限を掛ける。
      */
 
-    @NotBlank(message = "※タイトルを記入して下さい。")
-    @Size(min = 5, message = "5文字以上記入してください。")
+    @NotBlank(message = "")
+    @Size(min = 5,message = "タイトルは5文字以上記入してください。")
     private String title;
 
-    @NotBlank(message = "※内容を記入してください。")
-    @Size(min = 30,message = "30文字以上記入してください。")
+    @NotBlank(message = "")
+    @Size(min = 30,message = "内容は30文字以上記入してください。")
     private String contents;
 
     private Integer id;
