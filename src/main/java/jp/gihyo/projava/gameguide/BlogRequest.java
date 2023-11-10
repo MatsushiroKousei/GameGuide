6c581dcc82e463f8f3d8da15fcfdbffa3a12c823
Matsushiro <matsusiro12kousei@gmail.com>
2023-11-10 11:08:19 +0900

    バリデーション配置・色調整

--- src/main/java/jp/gihyo/projava/gameguide/BlogRequest.java
+++ src/main/java/jp/gihyo/projava/gameguide/BlogRequest.java
@@ -1,8 +1,10 @@
 package jp.gihyo.projava.gameguide;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.NonNull;
 
 /*
 入れ物
@@ -17,12 +19,12 @@ import lombok.Data;
 @Data
 public class BlogRequest {
 
-    @NotBlank(message = "")
+    @NotBlank(message = "空白は受付けません")
     @Size(min = 5,message = "5文字以上記入してください。")
     private String title;
 
-    @NotBlank(message = "")
-    @Size(min = 10,message = "10文字以上記入してください。")
+    @NotBlank(message = "空白は受付けません")
+    @Size(min = 10, message = "10文字以上記入してください。")
     private String contents;
 
     private Integer id;
